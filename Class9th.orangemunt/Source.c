836c8082e2c63b0129b2e014fa06e9a41c7dc75a
kbs6952 <jh750531@naver.com>
2024-03-26 19:42:10 +0900

    2024/03/26 // 문자열

--- Class9th.orangemunt/Source.c
+++ Class9th.orangemunt/Source.c
@@ -4,6 +4,54 @@
 
 void main()
 {
+#pragma region 문자열
+	// 연속적인 메모리 공간에 저장된 문자
+	// 변수의 집합입니다.
+
+//	const char* ptr = "Game";
+
+	// *ptr = 'T';
+	// % : 문자열을 출력하는 서식 지정자
+//	printf("ptr이 가리키는 값 : %s", ptr);
+
+//	ptr = "School";
+
+//	printf("ptr이 가리키는 값 : %s\n", ptr);
+
+	printf("%d", 2 % 3);
+
+
+
+	// 문자열의 경우 포인터를 이용하여 문자열
+	// 상수를 가리키도록 할 수 있으며, 문자열 상수는
+	// 데이터 영역의 읽기 전용 공간에 저장되기 때문에
+	// 문자열의 값을 변경할 수 없습니다.
+
+    //	char string[] = "Control";
+
+	// printf("string 배열의 크기 : %s\n", string);
+
+
+
+	// 문자열은 공백도 함께 메모리 공간에 포함하여 크기를
+	// 결정하며, 마지막 문자열의 끝을 알려주는 제어 문자가
+	// 추가됩니다.
+ 
+	//char name[] = "Ba\0rd";
+
+//	name[0] = "W";
+
+//	name = "Alistar"
+
+//	printf("name 배열의 값 : %s\n", name);
+
+	// 문자열의 경우 서로 연속적인 메모리 공간으로 연결되어
+	// 있지만, 문자 배열 사이에 무효의 문자를 넣게 되면 무효의
+	// 문자까지만 문자열을 출력합니다.
+
+	
+	
+
 #pragma region 주소 연산자
 	// 변수의 주소값을 반환하는 연산자입니다.
 
@@ -99,41 +147,41 @@ void main()
 	// 가리키고 있는 주소에 존재하는 값을 변경할 수
 	// 없도록 설정할 수 있습니다.
 
-int a = 10;
-int b = 20;
+	int a = 10;
+	int b = 20;
 
-const int* cptr = NULL;
+	const int* cptr = NULL;
 
-cptr = &a;
+	cptr = &a;
 
-printf("cptr의 값 : %p\n", cptr);
-// *cptr = 99;
+	printf("cptr의 값 : %p\n", cptr);
+	// *cptr = 99;
 
-cptr = 8;
+	cptr = 8;
 
-printf("cptr의 값 : %p\n", cptr);
-// *cptr = 99;
+	printf("cptr의 값 : %p\n", cptr);
+	// *cptr = 99;
 
-// 상수로 선언한 포인터는 해당 변수의 값을 변경할 수
-// 없지만, 다른 변수의 주소는 가리킬 수 있습니다.
+	// 상수로 선언한 포인터는 해당 변수의 값을 변경할 수
+	// 없지만, 다른 변수의 주소는 가리킬 수 있습니다.
 
 #pragma endregion
 
 #pragma region 포인터 상수화
 
-int v = 10;
-int w = 20;
+	int v = 10;
+	int w = 20;
 
-int* const reference = &v;
+	int* const reference = &v;
 
-*reference = 100;
+	*reference = 100;
 
-printf("v 변수의 값 : %b\n", v);
+	printf("v 변수의 값 : %b\n", v);
 
-// referenne = &v;
+	// referenne = &v;
 
-// 포인터 상수회는 새로운 메모리의 주소를
-// 저장할 수 있습니다.
+	// 포인터 상수회는 새로운 메모리의 주소를
+	// 저장할 수 있습니다.
 
 #pragma endregion  
 
@@ -181,3 +229,52 @@ int list[5];
 // 배열의 크기는 생략할 수 있으며, 초기화 목록에서 설정한
 // 요소에 따라 배열의 크기가 결정됩니다.
 #pragma endregion
+
+#pragma $region 약수
+ //  int value = 0;
+
+ //  scanf_s("value의 값 입력 : ");
+//   scanf_S("%d", &value);
+
+  // for (int i = 1; i <= value; i++)
+ //  {
+//	   if (value % i == 0)
+//	   {
+//		   printf("%d ", i);
+//	   }
+//   }
+
+#pragma $rigion 범용 포인터
+// 자료형이 정해지지 않은 상태로 모든 자료형을
+// 저장할 수 있는 포인터입니다.
+
+int data = 100;
+float health = 99.75;
+
+void* vptr = &data
+
+// *vptr = 1000;
+
+// 범용 포인터는 매모리 주소에 접근해서 값을
+// 변경할 수 없습니다.
+
+//*(int*)vptr = 1000;
+
+//printf("data 변수의 값 : %d\n", data);
+
+//vptr = &health;
+
+//*(float*)vptr = 100.0f;
+
+//printf("health 변수의 값 : %f\m", health);
+
+//vptr = "Visual Stduio";
+
+//printf("vptr이 가리키는 값 : %s\n", vptr);
+
+// 방음 포인터로 변수의 메모리에 접근하려면
+// 범용 포인터가 가리키는 변수의 자료형으로
+// 자료형 변환을 해주어야 합니다.
+
+
+#pragma #endregion
