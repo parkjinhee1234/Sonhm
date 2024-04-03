141f4ab9e3650fa2750f806f34c2429723621185
parkjinhee1234 <jh750531@naver.com>
2024-04-03 19:45:22 +0900

    2024/04/03 아스키 코드

---
+++ Class 15/Source.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <string.h>
+
+
+struct Goblin
+{
+	char grade;
+	int health;
+	const char* name;
+};
+
+struct GameObject
+{
+	int x;
+	double y;
+	short z;
+};
+
+int main()
+{
+#pragma region 구조처
+	// 여러 개의 변수를 하나의 집합으로 구조화한
+	// 다음 하나의 객체를 생성하는 것입니다.
+
+//	struct Goblin goblin1;
+
+//	goblin1.grade = 'A';
+//	goblin1.health = 100;
+//	goblin1.name = "난폭한 고블린";
+
+//	printf("goblin1의 주소 : %p\n", &goblin1);
+
+//	printf("goblin1.grade의 값 : % d\n",goblin1.grade);
+//	printf("goblin1.health의 값 : %d\n", goblin1.health);
+//	printf("goblin1.name의 값 : %p\n", &goblin1.name);
+
+//	struct Goblin goblin2 = {'S',200, "진화한 고블린"};
+
+//	    printf("goblin1.grade의 값 : % d\n", goblin2.grade);
+//		printf("goblin1.health의 값 : %d\n", goblin2.health);
+//		printf("goblin1.name의 값 : %p\n", &goblin2.name);
+
+	// 구조체의 데이터를 초기화할 때 초기화 리스트를 사용하여
+	// 초기화할 수 있으며, 왼쪽에서 오른쪽으로 초기화합니다.
+
+	// 구조체를 선언하기 전에 구조체는 메모리 공간이
+	// 생성되지 않으므로, 구조체 내부에 있는 데이터를
+	// 초기화할 수 있습니다.
+
+#pragma region 바이트 패딩
+	// 멤버 변수를 메모리에서 CPU로 읽을 때 한 번에 읽을 수
+	// 있도록, 컴파일러가 레지스타의 블록에 맞추어 바이트를
+	// 패딩해주는 최적화 작업입니다.
+
+	// struct GameObject gameObject;
+
+	// printf("GameObject의 크기 : %d\n", sizeof(gameObject));
+
+	// 구조체의 크기는 구조체를 구성하는 멤버 중에서 크기가
+	// 가장 큰 자료형의 배수가 되도록 정렬합니다.
+
+#pragma region 회문
+//	const char* string = "Level";
+//
+//	int size = strlen(string);
+//
+//	printf("size의 값 : %d\n", size);
+//
+//	int flag = 0;
+//
+//	for (int i = 0; i < size / 2; i++)
+//	{
+//		if (string[i] != string[size - 1 - i])
+//		{
+//			flag = 1;
+//			break;
+//		}
+//	}
+//	if (flag == 0)
+//	{
+//		printf("회문");
+//	}
+//	else
+//	{
+//		printf("회문아님");
+//	}
+
+#pragma #endrigion
+}
---
+++ Class16th.askicode/Source.c
@@ -0,0 +1,33 @@
+int main()
+{
+
+#pragma region ASCII 코드
+	// 미국 정보 교환 표준부호(ASC11)는 7비트로 문자를
+	// 표현하는 표준 코드입니다.
+
+
+	//for (int i = - 0; i < 26; i++)
+
+	//{
+		//printf("%c ", 'A' + i);
+	//}
+#pragma endregion
+
+#pragma region 자기 참조 구조제
+//	struct Node node1 = { 10, NULL };
+//	struct Node node2 = { 20, NULL };
+//	struct Node node3 = { 30, NULL };
+//	
+//	node1.next = &node2;
+//	node2.next = &node3;
+//	node3.next = NULL;
+//
+//	struct Node* currentNode = &node1;
+//
+//	while (currentNode != NULL)
+//	{
+//		printf("%d ", currentNode->data);
+//		currentNode = currentNode->next;
+//	}
+#pragma endreion
+}
