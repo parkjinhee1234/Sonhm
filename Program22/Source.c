fc41d106524019b3e64e39f70f73012b30116f5b
parkjinhee1234 <jh750531@naver.com>
2024-04-11 19:48:05 +0900

    2024/4/11 // 파일 입출력

--- Program22/Source.c
+++ Program22/Source.c
@@ -1,120 +1,74 @@
 #include <stdio.h>
-
-typedef struct Vector2
+#include <>
+#define SIZE 1000
+void Load(const char* name)
 {
-	int x;
-	int y;
+	FILE* file = fopen(name, "r");
 	
-}Vector2;
-
-typedef struct
-{
-	int x;
-	int y;
-	int z;
-}Vector3;
-
-
-int Factorial(int n)
-{
-	// if (n <= 1)
-	// {
-	// 	return 1;
-	// }
-	// 
-	// return n * Factorial(n - 1);
-	// 열거형 중간에 값을 변경하게 되면 그 다음의 값도
-	// 이전의 값에 영향을 받은 상태로 증가하게 됩니다.
-
+	char buffer[SIZE] = { 0, };
 	
+	fread(buffer, 1, SIZE, file);
+	
+	printf("%s", buffer);
+	
+	fclose(file);
+}
 
-	// 열거형은 0부터 시작하며, 1씩 증가합니다.
-
-
-}; 
 
 int main()
 {
-	//int n = 0;
-	//
-	//scanf_s("%d", &n);
-	//
-	//printf("%d! = %d\n", n, Factorial(n));
-	// enum STATUS status = ATTACK;
-	// 
-	// switch (status)
-	// {
-	// case IDLE: printf("대기 상태\n");
-	// 	break;
-	// case ATTACK: printf("공격 상태\n");
-	// 	break;
-	// case DIE: printf("죽은 상태\n");
-	// 	break;
-	// }
+#pragma region 파일 입출력
+	// 파일 입출력을 위해서는 파일을 열고 닫는 과정이 필요합니다.
+	// 파일을 열기 위해서는 fopen 함수를 사용합니다.
+	// fopen 함수는 파일의 경로와 파일의 모드를 인자로 받습니다.
+	// 파일의 경로는 파일의 경로를 문자열로 입력하면 됩니다.
+#pragma region AS
+	// r : 읽기 모드
+    // w : 쓰기 모드
+    // a : 추가 모드
+	// r+ : 읽기/쓰기 모드
+	// w+ : 읽기/쓰기 모드
+	// a+ : 읽기/쓰기 모드
 
+	// FILE* file = fopen("data.txt", "w");
+	// 
+	// fputs("Level : 5", file);
+	// fputs("Stage : 3", file);
+	// fputs("HP : 100", file);
+	// 
+	// fclose(file);
 
-#pragma region 이중 포인터
-	
-	// int data = 10;
-	// 
-	// int* ptr = &data;
-	// 
-	// int** ptr2 = &ptr;
-	// 
-	// printf("data변수의 값 : %d\n", data);
-	// 
-	// *ptr = 20;
-	// 
-	// printf("data 변수의 값 : %d\n", data);
-	// 
-	// **ptr2 = 30;
-	// 
-	// printf("data 변수의 값 : %d\n", data);
+	// fclese 함수는 파일의 포인터를 인자로 받습니다.
+    // 파일을 닫으면 파일의 포인터가 NULL로 초기화됩니다.
+	// 파일을 닫지 않으면 파일이 계속 열려있게 되어 
 
+	// FILE* file = fopen("data.txt", "r");
+	// 
+	// char buffer[SIZE] = { 0, };
+	// 
+	// fread(buffer, 1, SIZE, file);
+	// 
+	// printf("%s", buffer);
+	// 
+	// fclose(file);
+
+	// 파일의 내용을 읽어서 buffer에 저장합니다.
+
+	// fread 함수는 파일의 포인터, 읽을 데이터의 크기,
+	// 읽을 데이터의 개수, 파일의 포인터를 인자로 받습니다.
 #pragma endregion
 
-#pragma region 이차원 배열
-	// 배열의 요소로 또 다른 배열을 가지는 배열입니다.
-	int array2D[3][3] =
+	
+	while(1)
 	{
-		{1, 2, 3},
-		{4, 5, 6},
-		{7, 8, 9}
-	};
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			printf("%d ", array2D[i][j]);
-		}
-		printf("\n");
+		Load("data.txt");
+
+		system("cls");
 	}
-	printf("array2D[1][0]의 값 : %d\n", array2D[1][0]);
 
-	// 2차원 배열은 행과 열로 구분되며, 앞에 있는 배열은
-	// 행을 의미하고, 위에 있는 배열은 열을 의미합니다.
+	 
 
 
+	return 0;
 }
-
-	// Vector2 Vector2;
- 	// 
-	// Vector2.x = 10;
-	// Vector2.y = 20;
-	// 
-	// printf("Vecter2 x : %d\n", Vector2.x);
-	// printf("Vecter2 x : %d\n", Vector2.y);
-	// 
-	// Vector3 vector3 = { 5,10,15 };
-	// 
-	// printf("Vector3 x: %d\n", vector3.x);
-	// printf("Vector3 y: %d\n", vector3.y);
-	// printf("Vector3 z: %d\n", vector3.z);
-	// 
-	// 
-	// 
-	// printf("");
-
-
-
-
+	
